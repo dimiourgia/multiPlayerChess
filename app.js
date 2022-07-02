e51da929fe0095a63bb2f93411d8b45a00eea2d9
dimiourgia <ajayaxes318@gmail.com>
2022-07-03 00:52:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res)=>{
+    res.render('index.ejs');
+})
+
+app.get('/chess', (req, res)=>{
+    res.render('chess.ejs');
+})
+
+app.get('/puzzles', (req, res)=>{
+    res.render('puzzles.ejs');
+})
+
+app.get('/play', (req, res)=>{
+    res.render('play.ejs');
+})
+
+app.listen(3000);
